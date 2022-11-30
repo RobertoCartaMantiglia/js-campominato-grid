df6d82d7a46ef4109e51e3798a08e6c9b2dc8048
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-11-30 16:34:52 +0100

    make a square

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro
 // ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+
+// recupero il contenitore della griglia dal documento: 
+const containerGrid = document.querySelector("div.grid");
+
+// creo il quadrato/casella
+const gridSquare = document.createElement("div");
+
+// aggiungo la classe "square" che ho preparato in css per vederne il comportamento 
+gridSquare.classList.add("square");
+
+// procedo ad "appenderlo" al parent 
+containerGrid.append(gridSquare);
